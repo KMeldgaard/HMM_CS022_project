293044cc2f26a6174fd850db4b9175fb12c5306f
KMeldgaard <KMeldgaard@users.noreply.github.com>
2019-10-30 10:53:42 -0700

    Finished HMM class

--- scripts/first_implementation_from_paper/hmm.py
+++ scripts/first_implementation_from_paper/hmm.py
@@ -1,4 +1,68 @@
 """ HMM class
     hmm.py - 28-10-2019
-    Author: Kasper Meldgaard"""
+    Author: Kasper Meldgaard
+    ###
+    HMM class holds a hidden markov model (lambda) parameters.
+    N:  Number of states in model
+    M:  Number of different observations
+    A:  State transition matrix
+    B:  Observation probability matrix
+    pi: initial state
+    """
 
+DEBUG = "DEBUG:"
+
+# package dependencies
+import numpy as np
+# import pandas as pd
+import logging
+
+# DEBUG: on / off
+A_debug = logging.getLogger("A_debug")
+B_debug = logging.getLogger("B_debug")
+pi_debug = logging.getLogger("pi_debug")
+level = logging.DEBUG
+logging.basicConfig(level=level)
+# -- comment these in/out --#
+A_debug.disabled = True
+B_debug.disabled = True
+pi_debug.disabled = True
+
+
+class HMM:
+    def __init__(self, N, M):
+        self.__N__ = N
+        self.__M__ = M
+        self.A = np.array([self.__random_row__(N, A_debug) for i in range(N)])  # add N rows of length N
+        A_debug.debug("Array A:\n%s", self.A)  # debug
+
+        self.B = np.array([self.__random_row__(N, B_debug) for i in range(M)])
+        B_debug.debug("Array B:\n%s", self.B)  # debug
+
+        self.pi = np.array(self.__random_row__(N, pi_debug))
+        pi_debug.debug("pi: %s", self.pi)
+
+    def __random_row__(self, l, row_debug):
+        """Creates a row of l numbers with value normally distributed around 1/l"""
+        while True:
+            row = np.random.normal(loc=1 / l, scale=0.05,
+                                   size=(l - 1))  # normally distribute values around 1/l except last value
+            row_debug.debug("Row before last elem: %s", row)  # debug
+            row = np.append(row, 1 - np.sum(row))  # add the last element and ensure row sums to 1 (stochastic row)
+            row_debug.debug("Row after last elem: %s", row)  # debug
+            row_debug.debug("Sum of row: %s", sum(row))  # debug
+            if sum(row) == 1:
+                break
+        return row.tolist()  # a stochastic row of lenght l
+
+    def get_N(self):
+        return self.__N__
+
+    def get_M(self):
+        return self.__M__
+
+
+# test class:
+model = HMM(2, 27);
+
+# model.__random_row__(27)
