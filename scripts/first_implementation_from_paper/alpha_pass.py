702fc616985b0c71256b02e5cb2519c7c78adca4
KMeldgaard <KMeldgaard@users.noreply.github.com>
2019-11-06 12:06:03 -0800

    alpha-pass correction: added '()' in function call model.get_N()

--- scripts/first_implementation_from_paper/alpha_pass.py
+++ scripts/first_implementation_from_paper/alpha_pass.py
@@ -20,27 +20,27 @@ import logging
 
 
 def alpha_pass(model, obs_seq):
-    alpha = np.zeros((model.get_N, obs_seq.get_num_obs()))  # initialize alpha matrix
+    alpha = np.zeros((model.get_N(), obs_seq.get_num_obs()))  # initialize alpha matrix
     # c0 = 0
     c = np.zeros(obs_seq.get_num_obs())    # initialize c array with zeros
     # compute first row:
-    for n in range(model.get_N):
+    for n in range(model.get_N()):
         alpha[0, n] = model.pi[n] * model.B[obs_seq.obs[0], n]
         c[0] += alpha[0, n]
 
     # compute rest of alpha/c rows
     for t in range(1, (obs_seq.get_num_obs() - 1)):     # possible remove the '-1'
         # ct = 0
-        for n in range(model.get_N):  # n denoted i in paper
+        for n in range(model.get_N()):  # n denoted i in paper
             alpha[t, n] = 0
-            for i in range(model.get_N):  # denoted j in paper
+            for i in range(model.get_N()):  # denoted j in paper
                 alpha[t, n] += alpha[t - 1, i] * model.A[i, n]
             alpha[t, n] *= model.B[n, obs_seq.obs[t]]
             ct += alpha[t, n]
 
         # scale alpha:
         c[t] = 1 / c[t]
-        for n in range(model.get_N):
+        for n in range(model.get_N()):
             alpha[t, n] *= c[t]
             # end scale
 
