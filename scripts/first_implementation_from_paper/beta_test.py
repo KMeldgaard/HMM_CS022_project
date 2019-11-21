bccf3cbfa156ab89984dcedb76089af2fe37ea3e
MLuchsinger <luchsinger.max@gmail.com>
2019-11-20 16:22:29 -0800

    Switched a bunch of indexes which seemed to be wrong Runs without errors now
    Added beta_test to test beta_pass the same way alpha_pass got tested

--- scripts/first_implementation_from_paper/beta_test.py
+++ scripts/first_implementation_from_paper/beta_test.py
@@ -1,5 +1,29 @@
-# beta_test.py
-# Author: Maximiliano Luchsinger
-# Last Updated: 20.11.2019
-# Purpose: 
-# Program uses:
+""" Test script fom alpha-pass combined with model and obs objects
+    alpha_test.py - 20-11-2019
+    Author: Maximiliano Luchsinger"""
+
+from hmm import HMM
+from observation import Latin_observations
+from alpha_pass import alpha_pass
+from beta_pass import beta_pass
+
+# setup objects
+obs = Latin_observations("test_text.txt")
+model = HMM(2, 27);
+
+# attempt an alpha-pass
+alpha, c = alpha_pass(model, obs)
+
+# debug prints
+print("Model:\nA:\n", model.A, "\nB:\n", model.B, "\npi:\n", model.pi)
+print("Observations:\nSequence:\n", obs.obs, "\n# of obs:", obs.num_obs)
+print("Alpha matrix:\n", alpha)
+print("c vector:\n", c)
+
+
+
+
+# ----------------------------- BETA-PASS TEST ----------------------------------------------------
+beta = beta_pass(model, obs, c)
+# debug prints
+print("Beta matrix:\n", beta)
